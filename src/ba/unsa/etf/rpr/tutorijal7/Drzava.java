4a16382fc0a7ceb29151595cee3a0a4b47dbb377
amirnakic <anakic1@etf.unsa.ba>
2018-12-03 00:52:28 +0100

    Dodana je klasa Drzava po JavaBean specifikaciji.

---
+++ src/ba/unsa/etf/rpr/tutorijal7/Drzava.java
@@ -0,0 +1,62 @@
+package ba.unsa.etf.rpr.tutorijal7;
+
+import java.io.Serializable;
+
+public class Drzava implements Serializable {
+    private String naziv = "";
+    private int brojStanovnika = 0;
+    private double povrsina = 0;
+    private String jedinicaZaPovrsinu = "";
+    private Grad glavniGrad = new Grad();
+
+    public Drzava() {
+    }
+
+    public Drzava(String naziv, int brojStanovnika, double povrsina, String jedinicaZaPovrsinu, Grad glavniGrad) {
+        setNaziv(naziv);
+        setBrojStanovnika(brojStanovnika);
+        setPovrsina(povrsina);
+        setJedinicaZaPovrsinu(jedinicaZaPovrsinu);
+        setGlavniGrad(glavniGrad);
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public double getPovrsina() {
+        return povrsina;
+    }
+
+    public void setPovrsina(double povrsina) {
+        this.povrsina = povrsina;
+    }
+
+    public String getJedinicaZaPovrsinu() {
+        return jedinicaZaPovrsinu;
+    }
+
+    public void setJedinicaZaPovrsinu(String jedinicaZaPovrsinu) {
+        this.jedinicaZaPovrsinu = jedinicaZaPovrsinu;
+    }
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
+}
